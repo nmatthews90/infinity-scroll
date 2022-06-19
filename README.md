ef3d03ae5b1b58d108beb423285da3b0d14ab17c
Natasha Matthews <natashamatthews90@gmail.com>
2022-06-19 12:49:07 -0500

    added in the more photos after first load

--- README.md
+++ README.md
@@ -7,3 +7,7 @@
 [Google Font - Bebas Neue](https://fonts.google.com/specimen/Bebas+Neue?query=ba&preview.text=Infinite%20Scroll&preview.text_type=custom)
 
 [Unsplash API Documentation](https://unsplash.com/documentation)
+
+[W3Shools Scroll Event](https://www.w3schools.com/jsref/event_onscroll.asp)
+
+[W3Schools Load Event](https://www.w3schools.com/jsref/event_onload.asp)
--- script.js
+++ script.js
@@ -1,18 +1,90 @@
+const imageContainer = document.getElementById("image-container");
+const loader = document.getElementById("loader");
+
+let ready = false;
+let imagesLoaded = 0;
+let totalImages = 0;
+let photosArray = [];
+
+let isInitialLoad = true;
+
 //Unsplash API
-const count = 10;
+let initialCount = 5;
 const apiKey = "3Hmi3Ftw8aZrCu71GuJmjTYM7nEp71aKIUcXEthFGZU";
-const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
+let apiUrl = `https://api.unsplash.com/photos/random?client_id=${apiKey}&count=${initialCount}`;
+
+// NEW Block****
+function updateAPIURLWithNewCount(picCount) {
+  apiUrl = `https://api.unsplash.com/photos/random?client_id=${apiKey}&count=${picCount}`;
+}
+
+// Check if all images were loaded
+function imageLoaded() {
+  imagesLoaded++;
+  if (imagesLoaded === totalImages) {
+    ready = true;
+    loader.hidden = true;
+  }
+}
+
+// Helper Functions to Set Attributes on DOM Elements
+function setAttributes(element, attributes) {
+  for (const key in attributes) {
+    element.setAttribute(key, attributes[key]);
+  }
+}
+
+// Create Elements for Links and Photos, Add to Dom
+function displayPhotos() {
+  imagesLoaded = 0;
+  totalImages = photosArray.length;
+  //run fucntion for each object in photosArray
+  photosArray.forEach((photo) => {
+    // create <a> to link to Unsplash
+    const item = document.createElement("a");
+    setAttributes(item, {
+      href: photo.links.html,
+      target: "_blank",
+    });
+    // create <img> for photo
+    const img = document.createElement("img");
+    setAttributes(img, {
+      src: photo.urls.regular,
+      alt: photo.alt_description,
+      title: photo.alt_description,
+    });
+    // Event Listener, check when each is finished loading
+    img.addEventListener("load", imageLoaded);
+    // Put <img> inside <a>, then put both inside image-container
+    item.appendChild(img);
+    imageContainer.appendChild(item);
+  });
+}
 
 //Get phots form Unsplash API
 async function getPhotos() {
   try {
     const response = await fetch(apiUrl);
-    const data = await response.json();
-    console.log(data);
+    photosArray = await response.json();
+    displayPhotos();
+    if (isInitialLoad) {
+      updateAPIURLWithNewCount(30);
+      isInitialLoad = false;
+    }
   } catch (error) {
     // Catch Error Here
   }
 }
 
+// Check to see if Scrolling near bottom of page, Load More Photos
+window.addEventListener("scroll", () => {
+  if (
+    window.innerHeight + window.scrollY >= document.body.offsetHeight - 1000 &&
+    ready
+  ) {
+    ready = false;
+    getPhotos();
+  }
+});
 // On Load
 getPhotos();
